0c9020d36e5ac52025a9a149da653caa0a37f5b4
Devon Patey <devon.patey@gmail.com>
2017-06-01 16:07:28 -0600

    fixed all the things

--- dom_hw.js
+++ dom_hw.js
@@ -1,20 +1,20 @@
 document.getElementById('stubborn').addEventListener('click', function(e) {
-	console.log('this');
-	let heading = this.innerText;
-	alert("I'm right");
+	e.preventDefault();
+	document.getElementById('argue').innerHTML = "<h3>I am Right</h3>";
 });
 
 document.getElementById('more_stubborn').addEventListener('click', function(e) {
-	console.log('this');
-	let heading = this.innerText;
-	alert("No I'm right");
+	e.preventDefault();
+	document.getElementById('argue').innerHTML = "<h3> NO!! I am Right</h3>";
 });
 
+/*
 document.getElementById('hover1').addEventListener('mouseover', function(e) {
 	console.log('this');
 	let heading = this.innerText;
 	alert("Hey, I told you not to hover over me!");
 });
+*/
 
 document.addEventListener('keypress', function(e) {
 	let keypress = document.getElementById('keypress');
@@ -22,7 +22,14 @@ document.addEventListener('keypress', function(e) {
 });
 
 document.getElementById('submit').addEventListener('click', function(e){
-	document.getElementById('password').value === "12345678";
-	
+	e.preventDefault();
+	var password = document.getElementById('password').value;
+	var username = document.getElementById('username').value;
+	if (password === "12345678" && username.length <= 14) {
+		document.getElementById('info').innerHTML = "<h1>Congrats on knowing your username and password!</h1>";
+	}
+	else {
+		alert("Incorrect Username or Password");
+	}
 });
 
